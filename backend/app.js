73eb5ac15d8213b40fd0383f32ab4183e15a09ad
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-14 23:58:48 +0530

    frontend creation started

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const aiRoutes = require("./routes/ai.routes")
+const morgan = require("morgan")
 
 
 app.use(express.json())
+app.use(morgan())
 
 
 
