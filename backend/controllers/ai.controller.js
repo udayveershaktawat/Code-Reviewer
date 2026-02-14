a14fb7a1d8662a62e3c65bfdeadf3cf175d837bd
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-14 23:56:33 +0530

    backend completed

--- backend/controllers/ai.controller.js
+++ backend/controllers/ai.controller.js
@@ -1,13 +1,13 @@
 const aiService = require("../services/ai.service");
 
 module.exports.getReview = async (req, res) => {
-  const prompt = req.body.prompt;
+  const code = req.body.code;
 
-  if (!prompt) {
+  if (!code) {
     return res.status(400).send("Prompt is required");
   }
 
-  const response = await aiService(prompt);
+  const response = await aiService(code);
 
   res.send(response)
 };
