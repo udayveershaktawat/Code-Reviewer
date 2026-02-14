91ee7946fafa047a92e88dd694e5ed307d4b47c9
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-14 20:12:30 +0530

    initial commit and creating code reviewer

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const app = require("./app");
+
+
+
+app.listen(4000,()=>{
+    console.log("server started")
+})
---
+++ backend/services/ai,service.js
@@ -0,0 +1,12 @@
+import { GoogleGenAI } from "@google/genai";
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI(process.env.GOOGLE_GEMINI_KEY);
+const model = genAI.getGenerativeModel({model:"gemini-3-flash-preview"});
+
+
+async function generateContent(prompt){
+    const result = await model.generateContent(prompt)
+}
+
+
