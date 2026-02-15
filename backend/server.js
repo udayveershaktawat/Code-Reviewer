cb492895880073897955abcaa6be0b56e68c1a43
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-15 10:39:54 +0530

    u

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 
 
 
-app.listen(4000,()=>{
-    console.log("server started")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
